d77b55960d1ebf55c792569532d612a499b01e60
Dhruv Arora <dhruvarora91@gmail.com>
2021-11-24 15:52:02 +0530

    Added Password Hashing to Creating and Updating User

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -14,15 +14,3 @@ app.use(taskRouter)
 app.listen(port, () => {
   console.log('Server is up on port ' + port)
 })
-
-// * ADD THIS TO NOTES.ORG
-// const bcrypt = require('bcryptjs')
-// const hashPasswordFunction = async () => {
-//   const password = 'Test1234!@'
-//   const hashedPassword = await bcrypt.hash(password, 8)
-//   console.log(password)
-//   console.log(hashedPassword)
-//   const isMatch = await bcrypt.compare('Test1234!@', hashedPassword)
-//   console.log(isMatch)
-// }
-// hashPasswordFunction()
