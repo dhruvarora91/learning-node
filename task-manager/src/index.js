f4bd2f19e93753f827d8a746de1f1576213faff9
Dhruv Arora <dhruvarora91@gmail.com>
2021-11-26 17:35:43 +0530

    Generating Auth Token on login and creating a user

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -14,3 +14,16 @@ app.use(taskRouter)
 app.listen(port, () => {
   console.log('Server is up on port ' + port)
 })
+
+// const jwt = require('jsonwebtoken')
+
+// const myFunction = async () => {
+  
+//   const token = jwt.sign({ _id: 'abc123' }, 'thisisaprivatekey', { expiresIn: '7 days'})
+//   console.log(token)
+
+//   const data = jwt.verify(token, 'thisisaprivatekey')
+//   console.log(data)
+// }
+
+// myFunction()
--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
   name: {
@@ -19,6 +20,7 @@ const userSchema = mongoose.Schema({
   },
   email: {
     type: String,
+    unique: true,
     required: true,
     trim: true,
     lowercase: true,
@@ -38,9 +40,45 @@ const userSchema = mongoose.Schema({
         throw new Error('Password cannot contain the string \'Password\'')
       } 
     }
-  }
+  }, 
+  tokens: [{
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 })
 
+// methods are accessible on instance 
+// also called instance methods
+userSchema.methods.generateAuthToken = async function () {
+  const user = this
+  const token = jwt.sign({ _id: user._id.toString() }, 'thisisaprivatekey')
+  user.tokens = user.tokens.concat({ token: token })
+  await user.save()
+  return token
+}
+
+// statics are accessible on models 
+// also called model methods
+userSchema.statics.findByCredentials = async (email, password) => {
+
+  const user = await User.findOne({ email: email })
+  if(!user) {
+    // user doesn't exist with this email
+    throw new Error('Unable to login!')
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password)
+  if (!isMatch) {
+    // incorrect password
+    throw new Error('Unable to login!')
+  }
+  
+  return user
+}
+
+// Hash the plain text password before saving
 userSchema.pre('save', async function(next) {
   const user = this
 
